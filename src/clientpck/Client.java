7b325e6217b4c3cdc81ca6135cbc2b13dc6867eb
SMTPOverflow <36371618+SMTPOverflow@users.noreply.github.com>
2018-08-10 11:43:14 +0200

    implemented server broadcast

--- src/clientpck/Client.java
+++ src/clientpck/Client.java
@@ -1,6 +1,7 @@
 package clientpck;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
@@ -9,7 +10,38 @@ import util.SeializableMessage;
 
 public class Client {
 	private Socket localClientSocket;
-	
+
+	private void listenForServerBroadcast() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ObjectInputStream ois = null;
+				try {
+					ois = new ObjectInputStream(localClientSocket.getInputStream());
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+				try {
+					while (true) {
+						SeializableMessage message = (SeializableMessage) ois.readObject();
+						if (message != null) {
+							System.out.println(message.getMessage());
+						}
+						Thread.sleep(10);
+					}
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
 	public void sendMessage(SeializableMessage message) {
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(localClientSocket.getOutputStream());
@@ -18,11 +50,12 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Client(Socket localClientSocket) {
 		this.localClientSocket = localClientSocket;
+		this.listenForServerBroadcast();
 	}
-	
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 		Client client = new Client(new Socket("127.0.0.1", 8888));
 		client.sendMessage(new SeializableMessage(new EncryptedString("lol")));
--- src/serverpck/Server.java
+++ src/serverpck/Server.java
@@ -2,9 +2,11 @@ package serverpck;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 import util.EncryptedString;
 import util.Metadata;
@@ -12,8 +14,27 @@ import util.SeializableMessage;
 
 public class Server {
 	private ServerSocket localServerSocket;
+	private ArrayList<Socket> connectedClients = new ArrayList<>();
 	
-	public SeializableMessage acceptClientMessage(Socket remoteClient) {
+	private void sendBroadcastToClients(SeializableMessage messageToBroadcast) {
+		System.out.println("\t\t[SERVER] Sending Broadcast");
+		for (Socket socket : connectedClients) {
+			try {
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				oos.writeObject(messageToBroadcast);
+				System.out.print("\t\t\t[BROADCAST TO: " + socket.getInetAddress() + "] ");
+				
+				if(messageToBroadcast.getMetadata().getMessageType() == Metadata.TEXT) {
+					EncryptedString text = (EncryptedString)messageToBroadcast.getMessage();
+					System.out.println("Type: TEXT; Value: " + text.decrypt());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private SeializableMessage acceptClientMessage(Socket remoteClient) {
 		try {
 			ObjectInputStream ois = null;
 			int sleepTimeInS = 2;
@@ -44,7 +65,7 @@ public class Server {
 		return null;
 	}
 	
-	public void handleConnection(Socket remoteClient) {
+	private void handleConnection(Socket remoteClient) {
 		System.out.println("\t\t[CLIENT HANDLER: " + remoteClient.getInetAddress() + "] handling connection on " + remoteClient.getInetAddress());
 		new Thread(new Runnable() {
 			@Override
@@ -55,6 +76,7 @@ public class Server {
 					if(message.getMetadata().getMessageType() == Metadata.TEXT) {
 						EncryptedString text = (EncryptedString)message.getMessage();
 						System.out.println("\t\t\t[MESSAGE: " + remoteClient.getInetAddress() + "] Type: TEXT; Value: " + text.decrypt());
+						sendBroadcastToClients(message);
 					}
 				}
 			}
@@ -62,7 +84,7 @@ public class Server {
 	}
 	
 	public void start() {
-		System.out.println("[SERVER Server started");
+		System.out.println("[SERVER] Server started");
 		System.out.println("\t[ACCEPTION HANDLER] Waiting for clients to accept");
 		
 		new Thread(new Runnable() {
@@ -72,6 +94,7 @@ public class Server {
 					try {
 						Socket acceptedClient = localServerSocket.accept();
 						System.out.println("\t[ACCEPTION HANDLER] Client accepted: " + acceptedClient.getInetAddress());
+						connectedClients.add(acceptedClient);
 						handleConnection(acceptedClient);
 					} catch (IOException e) {
 						e.printStackTrace();
